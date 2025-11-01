a048ffc29f52faf494b50642639065f0671f0628
hasabatkd-hub <hasabat.kd@gmail.com>
2025-11-01 17:23:16 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./app/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+export default {
+  content: [
+    "./app//*.{js,jsx,ts,tsx}",
+    "./components//*.{js,jsx,ts,tsx}",
+  ],
   theme: { extend: {} },
-  plugins: []
+  plugins: [],
 };
